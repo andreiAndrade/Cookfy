cae23349aec0b6e1e61c4fec8564ea119191c469
andreiAndrade <andrei.9551@gmail.com>
2016-10-29 19:15:57 -0200

    Updated react

--- src/main/java/br/com/cookfyrest/resource/UserResource.java
+++ src/main/java/br/com/cookfyrest/resource/UserResource.java
@@ -6,16 +6,16 @@ import br.com.cookfyrest.repository.ReactRepository;
 import br.com.cookfyrest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.IllegalFormatException;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by Andrei Andrade on 29/10/2016.
@@ -25,8 +25,8 @@ import java.util.Objects;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    UserRepository userRepo;
-    ReactRepository reactRepo;
+    private UserRepository userRepo;
+    private ReactRepository reactRepo;
 
     @Autowired
     UserResource(UserRepository userRepo, ReactRepository reactRepo) {
@@ -34,16 +34,28 @@ public class UserResource {
         this.reactRepo = reactRepo;
     }
 
-    @RequestMapping(method = RequestMethod.GET,  path = "/reacts")
-    public List<React> getFavorities(@RequestParam(name = "id") Long id, @RequestParam(name = "react", required = false) String[] reactsType) {
+    @RequestMapping(method = RequestMethod.GET, path = "/reacts")
+    public ResponseEntity<List<React>> getFavorites(@RequestParam(name = "id") Long id, @RequestParam(name = "react", required = false) String[] reactsType) {
         List<React> reacts = new ArrayList<>();
-        if (Objects.nonNull(reactsType)) {
-            Arrays.stream(reactsType).forEach(rt -> reacts.addAll(reactRepo.findByReact(ReactDomain.valueOf(rt))));
-        }
-        return this.userRepo.findOne(id).getMyReacts();
-    }
 
-    public static void main(String[] args) {
-        System.out.println(ReactDomain.valueOf("LOE"));
+        if (reactsType == null || reactsType.length == 0) {
+            reacts = this.userRepo.findOne(id).getMyReacts();
+        } else {
+            for (String react : reactsType) {
+                ReactDomain reactDomain;
+
+                try {
+                    reactDomain = ReactDomain.valueOf(react);
+                } catch (IllegalFormatException ife) {
+                    continue;
+                }
+
+                reacts.addAll(reactRepo.findByReactAndUser(reactDomain, userRepo.findOne(id)));
+            }
+        }
+
+        return reacts.isEmpty() ?
+                new ResponseEntity<>(reacts, HttpStatus.NO_CONTENT) :
+                new ResponseEntity<>(reacts, HttpStatus.OK);
     }
 }
