269404c2dfd8ba5e306c611d57f71eee73998a6a
brunof <brunof@DBS-1111.dbserver.com.br>
2016-11-28 15:33:47 -0200

    Allowing CORS - connection from different domains

--- src/main/java/br/com/cookfyrest/resource/RecipeResource.java
+++ src/main/java/br/com/cookfyrest/resource/RecipeResource.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -147,11 +148,12 @@ public class RecipeResource {
         //return listOfPriorities;
     }
 
-
+    
     @RequestMapping(method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @CrossOrigin
     public Recipe createRecipe(
             @RequestBody RecipeDTO recipeDTO) {
 
