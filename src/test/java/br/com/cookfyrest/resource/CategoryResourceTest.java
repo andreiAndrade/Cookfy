3c7cb99769a0fa6d5ca1e964227e6acb1da51565
andreiAndrade <andrei.9551@gmail.com>
2016-11-28 23:39:20 -0200

    Removed test

--- src/test/java/br/com/cookfyrest/resource/CategoryResourceTest.java
+++ src/test/java/br/com/cookfyrest/resource/CategoryResourceTest.java
@@ -33,107 +33,107 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 //@WebAppConfiguration
 public class CategoryResourceTest {
 
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
-
-    private MockMvc mockMvc;
-
-    private String userName = "bdussault";
-
-    private HttpMessageConverter mappingJackson2HttpMessageConverter;
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    @Autowired
-    void setConverters(HttpMessageConverter<?>[] converters) {
-
-        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
-                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
-                .findAny()
-                .orElse(null);
-
-        assertNotNull("the JSON message converter must not be null",
-                this.mappingJackson2HttpMessageConverter);
-    }
-
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-
-//        this.bookmarkRepository.deleteAllInBatch();
-//        this.accountRepository.deleteAllInBatch();
+//    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+//            MediaType.APPLICATION_JSON.getSubtype(),
+//            Charset.forName("utf8"));
 //
-//        this.account = accountRepository.save(new Account(userName, "password"));
-//        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + userName, "A description")));
-//        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + userName, "A description")));
-    }
-
-//    @Test
-    public void readCategories() throws Exception {
-        mockMvc.perform(get("/categories")
-                .content(this.json(new Category()))
-                .contentType(contentType))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void readSingleCategory() throws Exception {
-        mockMvc.perform(get("/categories/1")
-                .content(this.json(new Category()))
-                .contentType(contentType))
-                .andExpect(status().isOk());
-    }
-
-//    @Test
-    public void singleCategoryNotFound() throws Exception {
-        mockMvc.perform(get("/categories/100")
-                .content(this.json(new Category()))
-                .contentType(contentType))
-                .andExpect(status().isNoContent());
-    }
-//    @Test
-//    public void readSingleBookmark() throws Exception {
-//        mockMvc.perform(get("/" + userName + "/bookmarks/"
-//                + this.bookmarkList.get(0).getId()))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(contentType))
-//                .andExpect(jsonPath("$.id", is(this.bookmarkList.get(0).getId().intValue())))
-//                .andExpect(jsonPath("$.uri", is("http://bookmark.com/1/" + userName)))
-//                .andExpect(jsonPath("$.description", is("A description")));
+//    private MockMvc mockMvc;
+//
+//    private String userName = "bdussault";
+//
+//    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+//
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
+//
+//    @Autowired
+//    void setConverters(HttpMessageConverter<?>[] converters) {
+//
+//        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+//                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+//                .findAny()
+//                .orElse(null);
+//
+//        assertNotNull("the JSON message converter must not be null",
+//                this.mappingJackson2HttpMessageConverter);
 //    }
 //
-//    @Test
-//    public void readBookmarks() throws Exception {
-//        mockMvc.perform(get("/" + userName + "/bookmarks"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(contentType))
-//                .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].id", is(this.bookmarkList.get(0).getId().intValue())))
-//                .andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
-//                .andExpect(jsonPath("$[0].description", is("A description")))
-//                .andExpect(jsonPath("$[1].id", is(this.bookmarkList.get(1).getId().intValue())))
-//                .andExpect(jsonPath("$[1].uri", is("http://bookmark.com/2/" + userName)))
-//                .andExpect(jsonPath("$[1].description", is("A description")));
+//    @Before
+//    public void setup() throws Exception {
+//        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+//
+////        this.bookmarkRepository.deleteAllInBatch();
+////        this.accountRepository.deleteAllInBatch();
+////
+////        this.account = accountRepository.save(new Account(userName, "password"));
+////        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/1/" + userName, "A description")));
+////        this.bookmarkList.add(bookmarkRepository.save(new Bookmark(account, "http://bookmark.com/2/" + userName, "A description")));
 //    }
 //
-//    @Test
-//    public void createBookmark() throws Exception {
-//        String bookmarkJson = json(new Bookmark(
-//                this.account, "http://spring.io", "a bookmark to the best resource for Spring news and information"));
-//
-//        this.mockMvc.perform(post("/" + userName + "/bookmarks")
-//                .contentType(contentType)
-//                .content(bookmarkJson))
-//                .andExpect(status().isCreated());
+////    @Test
+//    public void readCategories() throws Exception {
+//        mockMvc.perform(get("/categories")
+//                .content(this.json(new Category()))
+//                .contentType(contentType))
+//                .andExpect(status().isOk());
 //    }
-
-    protected String json(Object o) throws IOException {
-        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
-        this.mappingJackson2HttpMessageConverter.write(
-                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
-        return mockHttpOutputMessage.getBodyAsString();
-    }
+//
+////    @Test
+//    public void readSingleCategory() throws Exception {
+//        mockMvc.perform(get("/categories/1")
+//                .content(this.json(new Category()))
+//                .contentType(contentType))
+//                .andExpect(status().isOk());
+//    }
+//
+////    @Test
+//    public void singleCategoryNotFound() throws Exception {
+//        mockMvc.perform(get("/categories/100")
+//                .content(this.json(new Category()))
+//                .contentType(contentType))
+//                .andExpect(status().isNoContent());
+//    }
+////    @Test
+////    public void readSingleBookmark() throws Exception {
+////        mockMvc.perform(get("/" + userName + "/bookmarks/"
+////                + this.bookmarkList.get(0).getId()))
+////                .andExpect(status().isOk())
+////                .andExpect(content().contentType(contentType))
+////                .andExpect(jsonPath("$.id", is(this.bookmarkList.get(0).getId().intValue())))
+////                .andExpect(jsonPath("$.uri", is("http://bookmark.com/1/" + userName)))
+////                .andExpect(jsonPath("$.description", is("A description")));
+////    }
+////
+////    @Test
+////    public void readBookmarks() throws Exception {
+////        mockMvc.perform(get("/" + userName + "/bookmarks"))
+////                .andExpect(status().isOk())
+////                .andExpect(content().contentType(contentType))
+////                .andExpect(jsonPath("$", hasSize(2)))
+////                .andExpect(jsonPath("$[0].id", is(this.bookmarkList.get(0).getId().intValue())))
+////                .andExpect(jsonPath("$[0].uri", is("http://bookmark.com/1/" + userName)))
+////                .andExpect(jsonPath("$[0].description", is("A description")))
+////                .andExpect(jsonPath("$[1].id", is(this.bookmarkList.get(1).getId().intValue())))
+////                .andExpect(jsonPath("$[1].uri", is("http://bookmark.com/2/" + userName)))
+////                .andExpect(jsonPath("$[1].description", is("A description")));
+////    }
+////
+////    @Test
+////    public void createBookmark() throws Exception {
+////        String bookmarkJson = json(new Bookmark(
+////                this.account, "http://spring.io", "a bookmark to the best resource for Spring news and information"));
+////
+////        this.mockMvc.perform(post("/" + userName + "/bookmarks")
+////                .contentType(contentType)
+////                .content(bookmarkJson))
+////                .andExpect(status().isCreated());
+////    }
+//
+//    protected String json(Object o) throws IOException {
+//        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+//        this.mappingJackson2HttpMessageConverter.write(
+//                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+//        return mockHttpOutputMessage.getBodyAsString();
+//    }
 
 }
